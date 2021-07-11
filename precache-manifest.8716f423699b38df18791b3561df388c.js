887541c0a1e3fac0e213098567d4def5285e2ff4
Moni6662 <monisingh4522@gmail.com>
2021-07-12 00:55:46 +0530

    Updates

---
+++ precache-manifest.8716f423699b38df18791b3561df388c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8943a7ba3578c9eb9fd08abe921b54af",
+    "url": "/Job-listing/index.html"
+  },
+  {
+    "revision": "37ced482ac7d81175a37",
+    "url": "/Job-listing/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "b735343495c7aab9a180",
+    "url": "/Job-listing/static/js/2.f94499e3.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/Job-listing/static/js/2.f94499e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37ced482ac7d81175a37",
+    "url": "/Job-listing/static/js/main.05f94a8b.chunk.js"
+  },
+  {
+    "revision": "e54c69d014e894c156cf",
+    "url": "/Job-listing/static/js/runtime-main.5e630735.js"
+  }
+]);
